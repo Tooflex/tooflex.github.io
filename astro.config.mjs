fd016137a70fcac0fb832cd38c12a20c031499ba
Otourou Da Costa <otouroudacosta@gmail.com>
2024-01-13 04:08:40 +0100

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
 export default defineConfig({
   site: "https://tooflex.github.io",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-      cacheDir: "./.cache/image",
-      logLevel: "debug",
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
