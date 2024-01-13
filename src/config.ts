7a4b0588f24e848ec87c2ce372979e8fc5e96d45
Otourou Da Costa <otouroudacosta@gmail.com>
2024-01-13 03:37:19 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Otourou Da Costa | Senior iOS developer';
+export const SITE_DESCRIPTION = 'Hi, I\'m  Otourou Da Costa, a senior iOS developer and tech enthusiast based in Paris.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
