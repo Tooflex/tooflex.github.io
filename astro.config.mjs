91c1490ede0f472e9f124ef03c244176566a22bf
Otourou Da Costa <otouroudacosta@gmail.com>
2024-01-13 04:02:40 +0100

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://tooflex.github.io",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+      cacheDir: "./.cache/image",
+      logLevel: "debug",
+    }),
+  ],
 });
